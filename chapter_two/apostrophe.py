f585b0fbc01a0e3b0a753d133dc6df1029ac1128
martinminio <martin.minio@outlook.com>
2025-03-28 07:54:20 -0300

    comentarios en archivo

--- chapter_two/apostrophe.py
+++ chapter_two/apostrophe.py
@@ -1,2 +1,2 @@
-message="One of Python's strengths is its diverse community"
+message="One of Python's strengths is its diverse community" # the apostrophe in Python's is enclosed in double quotes
 print(message)
--- chapter_two/name.py
+++ chapter_two/name.py
@@ -1,24 +1,28 @@
 name="ada lovelace"
-print(name.title())
-print(name.upper())
-print(name.lower())
+print(name.title()) # prints the value in title case
+print(name.upper()) # prints the value in upper case
+print(name.lower()) # prints the value in lower case
 
 first_name="ada"
 last_name="lovelace"
-full_name=f"{first_name} {last_name}"
+full_name=f"{first_name} {last_name}" #f-strings are formatted strings. They are used to insert variables into strings
 print(full_name.title())
-print(f"Hello, {full_name.title()}")
+print(f"Hello, {full_name.title()}") # you can perform operations within f-strings inside the curly braces
 
-message=f"Hello, {full_name.title()}"
+message=f"Hello, {full_name.title()}" # you can also store the formatted string in a variable
 print(message)
 
-message=f"{full_name.title()} loves:\n\tSports\n\tProgramming\n\tGoing out with friends"
+message=f"{full_name.title()} loves:\n\tSports\n\tProgramming\n\tGoing out with friends" # \n is used to insert a new line and \t is used to insert a tab
 print(message)
 
 programming_language="Python "
-print(programming_language.rstrip())
+print(programming_language.rstrip()) # rstrip() removes the whitespace from the right side of the string
+programming_language=" Python"
+print(programming_language.lstrip()) # lstrip() removes the whitespace from the left side of the string
+programming_language=" Python "
+print(programming_language.strip()) # strip() removes the whitespace from both sides of the string
 
 gmail_url="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzQZTqBMGKnmnGkJnvJcsJGGdZld"
-gmail_url_no_prefix=gmail_url.removeprefix("https://")
+gmail_url_no_prefix=gmail_url.removeprefix("https://") # removeprefix() removes the specified prefix from the string
 print(gmail_url_no_prefix)
 
