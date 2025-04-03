9e8ead10a48a7235c02fb4bc07314838b148c6bc
martinminio <martin.minio@outlook.com>
2025-04-03 08:50:06 -0300

    primer práctica con listas. marcador

---
+++ chapter_three/lists.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'dale', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+print(bicycles[1].title())
+print(bicycles[-1].upper())
+message = f"\n\n My first bike was a {bicycles[0].title()}"
+print(message)
