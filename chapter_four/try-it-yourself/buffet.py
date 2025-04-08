1964796a667b74c7bc83e10f34ffd11f99d13f28
martinminio <martin.minio@outlook.com>
2025-04-07 23:43:06 -0300

    primera práctica con tuplas

---
+++ chapter_four/try-it-yourself/buffet.py
@@ -0,0 +1,8 @@
+foods = ('food1', 'food2', 'food3', 'food4', 'food5')
+for food in foods:
+    print(food)
+
+foods = ('food6', 'food7', 'food3', 'food4', 'food5')
+for food in foods:
+    print(food)
+    
