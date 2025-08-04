8a47f9e0eee5415f898bffec88788a1e8a20c63a
martinminio <martin.minio@outlook.com>
2025-08-04 18:12:24 -0300

    completacion del try it yourself del bloque if-statements del chapter five

---
+++ chapter_five/try-it-yourself/life_stages.py
@@ -0,0 +1,16 @@
+age = 5
+
+if age < 2 :
+    print("this person is a baby")
+elif age >= 2 and age < 4:
+    print("this person is a toddler")
+elif age >= 4 and age <= 13 :
+    print("this person is a kid")    
+elif age >= 13 and age <= 20 :
+    print("this person is a teenager")    
+elif age >= 20 and age <= 65 :
+    print("this person is an adult")
+else :
+    print("this person is an elder")
+
+        
