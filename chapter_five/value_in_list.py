a82bcfc61cdd3698e0eede8354e94e6c06450b7a
martinminio <martin.minio@outlook.com>
2025-08-05 16:24:48 -0300

    Actualizacion de archivo value_in_list para reflejar trabajo sobre listas y evaluacion de condiciones.

--- chapter_five/value_in_list.py
+++ chapter_five/value_in_list.py
@@ -5,4 +5,46 @@ banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 
 if user not in banned_users:
-    print(f"{user.title()}, you can post a response if you wish!")
+    print(f"{user.title()}, you can post a response if you wish!\n")
+
+########################################################
+# Checking for special items in list
+# Loop through the list of toppings and print a message for each one.
+# If the topping is 'onions', print a message that onions are out of stock.
+# Otherwise, print a message that the topping is being added to the pizza.
+
+toppings = ['mushrooms', 'onions', 'pineapple']
+for topping in toppings:
+    if topping == "onions":
+        print("Sorry, we're out of onions")
+    else:
+        print(f"Adding {topping} to your pizza")
+print("We finished making your pizza!\n")        
+
+## Checking a list is not empty
+toppings = []
+
+if toppings:
+    for topping in toppings:
+        if topping == "onions":
+            print("Sorry, we're out of onions")
+        else:
+            print(f"Adding {topping} to your pizza")
+    print("We finished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?\n")        
+
+## Using multiple lists
+available_toppings = ["mushrooms", "olives", "green peppers", "pepperoni", "pineapple", "extra cheese"]
+
+requested_toppings = ["mushrooms", "french fries", "extra cheese"]
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping} to your pizza")
+    else:
+        print(f"Sorry, we don't have {requested_topping}")
+
+print("We have finished your pizza!\n")
+
+
